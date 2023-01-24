4b3f2f8b988d0eba9b1e67cffa316121be84341f
m.kazinauskas@gmail.com <m.kazinauskas@gmail.com>
2023-01-25 00:21:08 +0200

    Color updates in theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ function CustomHeader() {
       </a>
     </nav>
     <div className="hidden lg:flex flex-col sm:flex-row sm:justify-center lg:justify-start gap-2.5 -ml-8">
-      <a href="#" className="inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">Ask a Question</a>
+      <a href="#" className="inline-block bg-indigo-600 hover:bg-indigo-800 active:bg-indigo-800 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">Ask a Question</a>
     </div>
     <div className="hidden lg:flex flex-col sm:flex-row sm:justify-center lg:justify-start gap-2.5 -ml-8">
       <a href="#" className="inline-block focus-visible:ring ring-indigo-300 text-gray-500 hover:text-indigo-500 active:text-indigo-600 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 py-3">Sign in</a>
